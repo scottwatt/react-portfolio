35825d0b2f616c2def04105067ec608c3820637c
Scott Wattenbarger <scottwattenbarger@gmail.com>
2022-11-29 18:10:38 -0800

    adds correct punctuation and changes some colors

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ function About() {
         I'm Scott, a full stack developer that loves to learn and is always trying to improve my study.
       </p>
       <p className="content">
-        I know that technology is always changing and improving. My goal is to keep ontop of the technologies and always better myself
+        I know that technology is always changing and improving. My goal is to keep ontop of the technologies and always better myself.
       </p>
     </div>
   );
